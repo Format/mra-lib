8ee053097e31dfc6eb579c274ec7702d6a79a715
mike andrews <mra@xoba.com>
2011-04-17 09:05:28 -0400

    some minor reformatting

--- src/com/xoba/util/xhtml/XHtmlUtils.java
+++ src/com/xoba/util/xhtml/XHtmlUtils.java
@@ -39,15 +39,18 @@ public class XHtmlUtils {
 			}
 		}
 
+		@Override
 		public void warning(SAXParseException exception) throws SAXException {
 			log(exception);
 		}
 
+		@Override
 		public void error(SAXParseException exception) throws SAXException {
 			log(exception);
 			hasErrors = true;
 		}
 
+		@Override
 		public void fatalError(SAXParseException exception) throws SAXException {
 			log(exception);
 			hasErrors = true;
@@ -67,6 +70,7 @@ public class XHtmlUtils {
 
 		private EntityResolver parent;
 
+		@Override
 		public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
 			if (publicId != null) {
 				if (publicId.equals("-//W3C//DTD XHTML 1.0 Transitional//EN")) {
