8cd683d00253b6d8a3658431d6b71c8075f25ba0
mike andrews <mra@xoba.com>
2011-09-03 07:36:00 -0400

    cleaned up a few trivial code issues (eclipse warnings)

--- src/com/xoba/util/data/AbstractManagedData.java
+++ src/com/xoba/util/data/AbstractManagedData.java
@@ -1,14 +1,8 @@
 package com.xoba.util.data;
 
-import java.io.File;
-import java.util.Date;
-
-import com.xoba.util.ILogger;
-import com.xoba.util.LogFactory;
-
 public abstract class AbstractManagedData<T> implements IManagedData<T> {
 
-	private static final ILogger logger = LogFactory.getDefault().create();
+	private static final long serialVersionUID = 1L;
 
 	private boolean useDailyManager;
 
@@ -17,26 +11,6 @@ public abstract class AbstractManagedData<T> implements IManagedData<T> {
 		DAILY_RELOADING_CACHE_MANAGER,
 
 		ID_BASED_CACHE_MANAGER;
-
-	}
-
-	public static void main(String... args) throws Exception {
-
-		IManagedData<String> testData = new AbstractManagedData<String>() {
-
-			public String createData() throws Exception {
-				return "test on " + new Date();
-			}
-
-			public String getID() {
-				return "test123";
-			}
-		};
-
-		IObjectManager om = new ObjectManager(false, new File("/tmp/om"));
-
-		String test = om.evaluateData(testData);
-		logger.debugf("got %s", test);
 
 	}
 
@@ -57,7 +31,8 @@ public abstract class AbstractManagedData<T> implements IManagedData<T> {
 		if (useDailyManager) {
 			a = CacheManagerUtil.createDailyCacheManager();
 		} else {
-			a = CacheManagerUtil.createIDBasedCacheManager(DAYS_EXPIRATION_FOR_ID_BASED_MANAGER);
+			a = CacheManagerUtil
+					.createIDBasedCacheManager(DAYS_EXPIRATION_FOR_ID_BASED_MANAGER);
 		}
 
 		return a;
